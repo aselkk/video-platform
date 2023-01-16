dfe047182a8fab163d6f6083d6827eeb3ed7e382
Asel Kk <aselkurmanbekk@gmail.com>
2023-01-16 13:43:18 +0600

    bug: fix some bugs

--- lambdas/user/getUsers.js
+++ lambdas/user/getUsers.js
@@ -1,14 +1,21 @@
-const { DynamoDB } = require("aws-sdk")
+const AWS = require('aws-sdk')
 
-const db = new DynamoDB.DocumentClient()
-const {table_name} = process.env
+const docClient = new AWS.DynamoDB.DocumentClient();
+const { table_name } = process.env;
+const headers = {
+    "content-type": "application/json",
+};
 
-module.exports.handler = async (event) => {
-	const userList = await db
-		.scan({
-			table_name,
-		})
-		.promise()
+module.exports.handler = async () => {
+    const output = await docClient
+        .scan({
+            TableName: table_name,
+        })
+        .promise();
 
-	return { statusCode: 200, body: JSON.stringify(userList) }
-} 
+    return {
+        statusCode: 200,
+        headers,
+        body: JSON.stringify(output.Items),
+    };
+};
--- lambdas/user/verification.js
+++ lambdas/user/verification.js
@@ -3,22 +3,21 @@ const { sendResponse } = require("../index");
 const cognito = new AWS.CognitoIdentityServiceProvider();
 
 module.exports.handler = async (event) => {
+	const { email, code } = JSON.parse(event.body);
+	const { client_id } = process.env
+	const params = {
+			ClientId: client_id,
+			Username: email,
+			ConfirmationCode: code,
+	};
 	try {
-		const { email, code } = JSON.parse(event.body);
-		const {client_id} = process.env
-		const params = {
-				ClientId: client_id,
-				Username: email,
-				ConfirmationCode: code,
-		};
-		const response = await cognito.confirmSignUp(params, (err) => {
-				if (err) {
-					return sendResponse(400, { message: "code is invalid" });
-				}
+		await cognito.confirmSignUp(params, (err) => {
+			if (err) {
+				return sendResponse(400, { message: "code is invalid" });
+			}
 		});
-		return sendResponse(200, { message: "success" });
+		return sendResponse(200, { message: "success", params });
 	} catch (error) {
-		const message = error.message ? error.message : "Internal server error";
-		return sendResponse(500, { message });
-    }
+		return sendResponse(500, { message: 'params', params });
+  }
 };
